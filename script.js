601bb1a2c8564f8ea68144e0eb5665ed9f1ec5e8
Racyn Ibaya <racynibaya@outlook.com>
2022-07-27 12:07:20 -0400

    adding the loader

--- script.js
+++ script.js
@@ -1,25 +1,54 @@
-//
-
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
 let photosArr = [];
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 
-const count = 10;
+const count = 30;
 const apiKey = 'GarSNaDMsojS9fi2DsREH7HLrfkwCsrYhCMkx8feLaE';
 
 const apiURL = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
 
+const setAttributes = function (element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+};
+
+const imageLoaded = function () {
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+  }
+};
+
 const displayPhoto = function () {
+  imagesLoaded = 0;
+  totalImages = photosArr.length;
+  console.log('total images', totalImages);
   photosArr.forEach(photo => {
-    console.log(photo);
     const item = document.createElement('a');
-    item.setAttribute('href', photo.links.html);
+
+    setAttributes(item, {
+      href: photo.links.html,
+      target: '_blank',
+    });
 
     const img = document.createElement('img');
     img.setAttribute('src', photo.urls.regular);
     img.setAttribute('alt', photo.alt_description);
     img.setAttribute('title', photo.alt_description);
+
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+
+    img.addEventListener('load', imageLoaded);
 
     item.appendChild(img);
     imageContainer.appendChild(item);
@@ -36,5 +65,15 @@ const getPhoto = async function () {
     // Catch error
   }
 };
+
+window.addEventListener('scroll', () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready === true
+  ) {
+    ready = false;
+    getPhoto();
+  }
+});
 
 getPhoto();
